879587440c6c933a39a78df885958fb007549aa1
Lucas GAUTHERON <lucas.gautheron@gmail.com>
2016-08-06 23:45:56 +0200

    WIMP simulation improvement
    Taking into account the DoF derivate term

--- simulations/WIMP/wimp.m
+++ simulations/WIMP/wimp.m
@@ -6,6 +6,10 @@ gS[x_] := 10.+90.*(1.+Tanh[mass/x-0.5])
 Yeq[x_] := 0.145 * gX/gS[x] * x^1.5 * Exp[-x]
 lambda := 2.76*10^9*mass*sigmav
 
+(*dgS[x_?NumericQ] := Evaluate[D[Log[10.+90.*(1.+Tanh[x-0.5])],x]]/x*)
+dgS[x_?NumericQ] := (90*Sech[x-0.5]/(10.+90.*(1.+Tanh[x-0.5])))/x
+dgSx[x_] := dgS[mass/x]
+
 xmin := 1
 xmax := 2000
 cell[x_, y_] := 1.*Evaluate[xmin+x*(xmax-xmin)/Npts]
@@ -18,31 +22,31 @@ testing = Array[cell, {Npts, 6}, {0, 0}]
 For[i = 0, i <= Npts, i++, testing[[i,2]] = Evaluate[Yeq[testing[[i,1]]]/Yeq[1.]]]
 
 mass = 1.
-sol = NDSolve[{W'[x] == (lambda/(x*x)) * 1 * (Exp[2*Log[Yeq[x]]-W[x]] - Exp[W[x]] ) , W[xmin] == Log[Yeq[xmin]]}, W, {x, xmin, xmax}]
+sol = NDSolve[{W'[x] == (lambda/(x*x)) * (1.+3.*dgSx[x]) * (Exp[2*Log[Yeq[x]]-W[x]] - Exp[W[x]] ) , W[xmin] == Log[Yeq[xmin]]}, W, {x, xmin, xmax}]
 For[i = 0, i <= Npts, i++, testing[[i,3]] = mass*Evaluate[Exp[W[testing[[i,1]]]]/Yeq[1.] /. sol][[1]]]
 
 Clear[sol];
 Clear[W];
 mass = 100.
-sol = NDSolve[{W'[x] == (lambda/(x*x)) * 1 * (Exp[2*Log[Yeq[x]]-W[x]] - Exp[W[x]] ) , W[xmin] == Log[Yeq[xmin]]}, W, {x, xmin, xmax}]
+sol = NDSolve[{W'[x] == (lambda/(x*x)) * (1.+3.*dgSx[x]) * (Exp[2*Log[Yeq[x]]-W[x]] - Exp[W[x]] ) , W[xmin] == Log[Yeq[xmin]]}, W, {x, xmin, xmax}]
 For[i = 0, i <= Npts, i++, testing[[i,4]] = mass*Evaluate[Exp[W[testing[[i,1]]]]/Yeq[1.] /. sol][[1]]]
 
 Clear[sol];
 Clear[W];
 mass = 1000.
-sol = NDSolve[{W'[x] == (lambda/(x*x)) * 1 * (Exp[2*Log[Yeq[x]]-W[x]] - Exp[W[x]] ) , W[xmin] == Log[Yeq[xmin]]}, W, {x, xmin, xmax}]
+sol = NDSolve[{W'[x] == (lambda/(x*x)) * (1.+3.*dgSx[x]) * (Exp[2*Log[Yeq[x]]-W[x]] - Exp[W[x]] ) , W[xmin] == Log[Yeq[xmin]]}, W, {x, xmin, xmax}]
 For[i = 0, i <= Npts, i++, testing[[i,5]] = mass*Evaluate[Exp[W[testing[[i,1]]]]/Yeq[1.] /. sol][[1]]]
 
 Clear[sol];
 Clear[W];
 mass = 10000.
-sol = NDSolve[{W'[x] == (lambda/(x*x)) * 1 * (Exp[2*Log[Yeq[x]]-W[x]] - Exp[W[x]] ) , W[xmin] == Log[Yeq[xmin]]}, W, {x, xmin, xmax}]
+sol = NDSolve[{W'[x] == (lambda/(x*x)) * (1.+3.*dgSx[x]) * (Exp[2*Log[Yeq[x]]-W[x]] - Exp[W[x]] ) , W[xmin] == Log[Yeq[xmin]]}, W, {x, xmin, xmax}]
 For[i = 0, i <= Npts, i++, testing[[i,6]] = mass*Evaluate[Exp[W[testing[[i,1]]]]/Yeq[1.] /. sol][[1]]]
 
 Clear[sol];
 Clear[W];
 mass = 50000.
-sol = NDSolve[{W'[x] == (lambda/(x*x)) * 1 * (Exp[2*Log[Yeq[x]]-W[x]] - Exp[W[x]] ) , W[xmin] == Log[Yeq[xmin]]}, W, {x, xmin, xmax}]
+sol = NDSolve[{W'[x] == (lambda/(x*x)) * (1.+3.*dgSx[x]) * (Exp[2*Log[Yeq[x]]-W[x]] - Exp[W[x]] ) , W[xmin] == Log[Yeq[xmin]]}, W, {x, xmin, xmax}]
 For[i = 0, i <= Npts, i++, testing[[i,6]] = mass*Evaluate[Exp[W[testing[[i,1]]]]/Yeq[1.] /. sol][[1]]]
 
 (*LogLogPlot[Evaluate[Exp[W[x]]/Yeq[1.] /. s], {x, xmin, xmax}, PlotRange -> All]*)
