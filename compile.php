5baf74ff0108cecfaacd701b7d6b0b693a32a376
Lucas GAUTHERON <lucas.gautheron@gmail.com>
2016-08-12 23:55:55 +0200

    compile script fix
    removed call of now removed references.xml file

--- compile.php
+++ compile.php
@@ -17,7 +17,6 @@ function strip_decl($str)
 $files[0] = strip_decl(file_get_contents("data/events.xml"));
 $files[1] = strip_decl(file_get_contents("data/contents.xml"));
 $files[2] = strip_decl(file_get_contents("data/appendices.xml"));
-$files[3] = strip_decl(file_get_contents("data/references.xml"));
 
 file_put_contents('data/cache', "<?xml version=\"1.0\" encoding=\"UTF-8\"?><root>{$files[0]}{$files[1]}{$files[2]}{$files[3]}</root>");
 
