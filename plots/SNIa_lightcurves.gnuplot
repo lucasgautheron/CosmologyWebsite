c326b652a034fdb15a327460b00ff57299b3da5a
Lucas GAUTHERON <lucas.gautheron@gmail.com>
2016-09-16 12:53:19 +0200

    More supernovae work. Include lightcurves

---
+++ plots/SNIa_lightcurves.gnuplot
@@ -0,0 +1,8 @@
+set xrange [-50:300]
+set yrange [:] reverse
+
+set xlabel 'Temps (en jours) depuis le maximum de luminosite'
+set ylabel 'Magnitude absolue M_{B}'
+
+plot 'data/SN1999ee_lightcurve.res' u 1:2 t 'SN1999ee' ps 0.5, 'data/SN2011fe_lightcurve.res' u 1:2 t 'SN2011fe' ps 0.5, 'data/SN1991bg_lightcurve.res' u 1:2 t 'SN1991bg' ps 0.5, 'data/SN2014J_lightcurve.res' u 1:2 t 'SN2014J' ps 0.5, 'data/SN2012fr_lightcurve.res' u 1:2 t 'SN2012fr' ps 0.5
+
---
+++ simulations/supernovae/fetch.php
@@ -0,0 +1,74 @@
+<?php
+$supernovae = array('SN1991bg', 'SN1999ee', 'SN2011fe', 'SN2014J', 'SN2012fr');
+
+function getlightcurve($data, $band)
+{
+    $max_absmag = $max_appmag = 0;
+
+    foreach($data['maxband'] as $n => $b)
+    {
+        if($b['value'] == $band)
+        {
+            $max_absmag = $data['maxabsmag'][$n]['value'];
+            $max_appmag = $data['maxappmag'][$n]['value'];
+        }
+    }
+
+    $entries = array();
+    $peak_mag = 1000;
+    $peak_time = 0;
+
+    foreach($data['photometry'] as $entry)
+    {
+        if($entry['band'] == $band)
+        {
+            $entry['time'] = is_array($entry['time']) ? array_sum($entry['time'])/count($entry['time']) : $entry['time'];
+            $entries[] = $entry;
+
+            if($entry['magnitude'] < $peak_mag)
+            {
+                $peak_mag = $entry['magnitude'];
+                $peak_time = $entry['time'];
+            }
+        }
+
+
+    }
+
+    foreach($entries as &$entry)
+    {
+        @$entry['magnitude'] += $max_absmag - $max_appmag;
+        $entry['time_since_maximum'] = (float)$entry['time'] - (float)$peak_time;
+    }
+
+    return $entries;
+}
+
+
+$curves = array();
+foreach($supernovae as $supernova)
+{
+    $json = "../../tmp/$supernova.json";
+    if(!file_exists($json))
+    {
+        file_put_contents($json, file_get_contents("https://sne.space/sne/$supernova.json"));
+    }
+
+    $data = json_decode(file_get_contents($json), true);
+    $data = $data[$supernova];
+
+//print_r($data);
+
+    $z = $data['redshift'];
+
+    $curves[$supernova]['B'] = getlightcurve($data, 'B');
+}
+
+
+foreach($curves as $SN => $curve)
+{
+    $fp = fopen("../../plots/data/{$SN}_lightcurve.res", "w+");
+    foreach($curve['B'] as $datapoint) fwrite($fp, "{$datapoint['time_since_maximum']} {$datapoint['magnitude']}\n");
+}
+fclose($fp);
+?>
