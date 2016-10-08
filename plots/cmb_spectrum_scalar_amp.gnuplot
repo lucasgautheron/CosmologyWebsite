590747e4f43e8844e547825ed63e3d71835d69d6
Lucas GAUTHERON <lucas.gautheron@gmail.com>
2016-10-09 00:44:01 +0200

    More CMB plots

---
+++ plots/cmb_spectrum_scalar_amp.gnuplot
@@ -0,0 +1,12 @@
+files = system('ls data/output_scalar_amp*.res')
+samplefile = system("ls data/output_scalar_amp*.res | head -1| awk '{print $NF}'")
+param = system('head -n 1 "' . samplefile . '" | cut -d "#" -f 2 | cut -d "=" -f 1')
+
+set xlabel param
+set ylabel ''
+
+set xrange [0:2500]
+
+set logscale y
+
+plot for [file in files] file u 1:2 title system("head -n 1 '".file."' | cut -d '#' -f 2") w l
--- simulations/CMB/run.php
+++ simulations/CMB/run.php
@@ -34,10 +34,14 @@ function build_template($param, $label, $values)
 }
 
 
-build_template("scalar_amp(1)", "{/Symbol D} R^{2}", array(2.1e-10, 2.1e-9, 2.1e-8));
+build_template("scalar_amp(1)", "{/Symbol d} R^{2}", array(2.1e-10, 2.1e-9, 2.1e-8));
 
 build_template("scalar_spectral_index(1)", "n_{s}", array(0.9, 0.96, 1.0, 1.1));
 
 build_template("ombh2", "{/Symbol O}_{b} h^2", array(0.01, 0.0226, 0.03));
 
+build_template("omch2", "{/Symbol O}_{cdm} h^2", array(0.05, 0.112, 0.15));
+
 build_template("re_optical_depth", "{/Symbol t}", array(0.01, 0.09, 0.18));
+
+build_template("hubble", "H_{0}", array(60, 70, 80));
